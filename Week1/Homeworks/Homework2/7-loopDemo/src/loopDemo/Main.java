8d745567314b8c7d817020c1422d804187145aeb
emrecanstk <emrecanstk@gmail.com>
2022-09-15 14:54:46 +0300

    Add loopDemo

---
+++ Week1/Homeworks/Homework2/7-loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,27 @@
+package loopDemo;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 11 12 13
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// for
+		for(int i=1;i<10;i++) {
+			System.out.println(i);
+		}
+		System.out.println("for döngüsü bitti.");
+		
+		// while
+		int i=1;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("while döngüsü bitti.");
+		
+	}
+
+}
