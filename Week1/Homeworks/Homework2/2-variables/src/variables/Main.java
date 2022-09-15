7fdf3a30fdb5d298b3ba6d2f63ae758cc0011b9f
emrecanstk <emrecanstk@gmail.com>
2022-09-15 14:53:14 +0300

    Add variables

---
+++ Week1/Homeworks/Homework2/2-variables/src/variables/Main.java
@@ -0,0 +1,19 @@
+package variables;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 6
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı: ";
+		System.out.println("Öğrenci sayısı: "+ ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		
+		
+
+	}
+
+}
