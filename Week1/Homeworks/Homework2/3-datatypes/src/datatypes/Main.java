2d6ad595a5c85a95a3aca7499c0c5344f836463e
emrecanstk <emrecanstk@gmail.com>
2022-09-15 14:53:28 +0300

    Add datatypes

---
+++ Week1/Homeworks/Homework2/3-datatypes/src/datatypes/Main.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 7
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		double b = 10.50;
+		char c = 'T';
+		boolean d = false;
+		
+
+	}
+
+}
