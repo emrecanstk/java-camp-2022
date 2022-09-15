a2ae34e4e25a500bb416a41efc439003d2222854
emrecanstk <emrecanstk@gmail.com>
2022-09-15 14:52:57 +0300

    Add javaHelloWorld

---
+++ Week1/Homeworks/Homework2/1-javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,16 @@
+package javaHelloWorld;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 5
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		System.out.println("Emre Can Satık");
+
+	}
+
+}
