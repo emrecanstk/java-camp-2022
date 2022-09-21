9aa24424ad6aa49ab5a41fadd040599f0163d5c6
emrecanstk <emrecanstk@gmail.com>
2022-09-21 23:24:45 +0300

    Add getters and setters

--- Week2/Codes/oop1/src/oop1/Customer.java
+++ Week2/Codes/oop1/src/oop1/Customer.java
@@ -5,6 +5,28 @@ public class Customer {
 	private String customerNumber;
 	private String phone;
 
-	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCustomerNumber() {
+		return customerNumber;
+	}
+
+	public void setCustomerNumber(String customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
