b5ea7aa306df0b7ffd591060e77cb43cc489722d
emrecanstk <emrecanstk@gmail.com>
2022-09-21 22:43:57 +0300

    Add getters and setters

--- Week2/Codes/oop1/src/oop1/Product.java
+++ Week2/Codes/oop1/src/oop1/Product.java
@@ -1,9 +1,50 @@
 package oop1;
 
 public class Product {
-	String name;
-	double unitPrice;
-	double discount;
-	String imageUrl;
-	int unitsInStock;
+	private String name;
+	private double unitPrice;
+	private double discount;
+	private String imageUrl;
+	private int unitsInStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
 }
