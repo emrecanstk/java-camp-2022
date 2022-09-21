e960f3759c26205b345198b8197dcb346d9361a6
emrecanstk <emrecanstk@gmail.com>
2022-09-21 23:40:38 +0300

    Add customer array

--- Week2/Codes/oop1/src/oop1/Main.java
+++ Week2/Codes/oop1/src/oop1/Main.java
@@ -28,28 +28,28 @@ public class Main {
 		product3.setImageUrl("bilmemne3.jpg");
 
 		Product[] products = { product1, product2, product3 };
-		
+
 		System.out.println("<ul>");
 		for (Product product : products) {
-			System.out.println("<li>"+product.getName()+"</li>");
+			System.out.println("<li>" + product.getName() + "</li>");
 		}
 		System.out.println("</ul>");
-		
+
 		IndividualCustomer individualCustomer = new IndividualCustomer();
 		individualCustomer.setId(1);
 		individualCustomer.setPhone("051111111111");
 		individualCustomer.setCustomerNumber("124841244");
 		individualCustomer.setFirstName("Emre Can");
 		individualCustomer.setLastName("Satık");
-		
+
 		CorporateCustomer corporateCustomer = new CorporateCustomer();
 		corporateCustomer.setId(2);
 		corporateCustomer.setCompanyName("Kodlama.io");
 		corporateCustomer.setPhone("053444444");
 		corporateCustomer.setTaxNumber("1111111111111");
 		corporateCustomer.setCustomerNumber("15545113");
-		
-		
+
+		Customer[] customers = { individualCustomer, corporateCustomer };
 	}
 
 }
