aa439bedebb0fc0bbe4ba173e4f1ce8ad7e68912
emrecanstk <emrecanstk@gmail.com>
2022-09-21 22:49:13 +0300

    Update Main about getters setters

--- Week2/Codes/oop1/src/oop1/Main.java
+++ Week2/Codes/oop1/src/oop1/Main.java
@@ -3,22 +3,36 @@ package oop1;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		String mesaj = "Vade oranı";
-		
+
 		Product product1 = new Product();
+		product1.setName("Delonghi Kahve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitsInStock(4);
+		product1.setImageUrl("bilmemne1.jpg");
+
+		Product product2 = new Product();
+		product2.setName("Simag Kahve Makinesi");
+		product2.setDiscount(10);
+		product2.setUnitPrice(9000);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("bilmemne2.jpg");
+
+		Product product3 = new Product();
+		product3.setName("Kitchen Aid Kahve Makinesi");
+		product3.setDiscount(4);
+		product3.setUnitPrice(6700);
+		product3.setUnitsInStock(8);
+		product3.setImageUrl("bilmemne3.jpg");
+
+		Product[] products = { product1, product2, product3 };
 		
-		   // set value
-		product1.name = "Delonghi Kahve Makinesi";
-		product1.unitPrice = 7500;
-		product1.discount = 7;
-		product1.unitsInStock = 3;
-		product1.imageUrl = "bilmemne.jpg";
-		
-		   // get value
-		System.out.println(product1.name);
-		
-		
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+
 	}
 
 }
