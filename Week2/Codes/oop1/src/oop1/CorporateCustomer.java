01cab7feff6ee2d3bbf10bc78c18e1a7b688cf5f
emrecanstk <emrecanstk@gmail.com>
2022-09-21 23:38:47 +0300

    Update CorporateCustomer

--- Week2/Codes/oop1/src/oop1/CorporateCustomer.java
+++ Week2/Codes/oop1/src/oop1/CorporateCustomer.java
@@ -1,5 +1,23 @@
 package oop1;
 
-public class CorporateCustomer {
+public class CorporateCustomer extends Customer {
+	private String companyName;
+	private String taxNumber;
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
 
 }
