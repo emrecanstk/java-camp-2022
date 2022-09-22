5c77123e919ee68435786ae0304c6bfffe92c6ad
emrecanstk <emrecanstk@gmail.com>
2022-09-22 19:23:22 +0300

    Update classes

--- Week2/Homeworks/Homework2/classes/src/classes/Main.java
+++ Week2/Homeworks/Homework2/classes/src/classes/Main.java
@@ -3,6 +3,13 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager = customerManager2;
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
 		
 
 	}
