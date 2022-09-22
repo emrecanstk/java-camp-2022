4ea498abd8c3eb1b78dfe3194becf993fb8bffdb
emrecanstk <emrecanstk@gmail.com>
2022-09-22 21:01:40 +0300

    Add methodOverLoading

---
+++ Week2/Homeworks/Homework2/methodOverloading/src/methodOverloading/DortIslem.java
@@ -0,0 +1,17 @@
+package methodOverloading;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 35
+
+public class DortIslem {
+
+	public int topla(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+	}
+	
+	public int topla(int sayi1, int sayi2, int sayi3) {
+		return sayi1 + sayi2 + sayi3;
+	}
+	
+}
---
+++ Week2/Homeworks/Homework2/methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,15 @@
+package methodOverloading;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 35
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		dortIslem.topla(1, 7);
+		dortIslem.topla(4, 5, 9);
+	}
+
+}
