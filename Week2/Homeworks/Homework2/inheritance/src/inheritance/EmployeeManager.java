968c8e935f73396e54ddee68eacf586089a5edb4
emrecanstk <emrecanstk@gmail.com>
2022-09-22 21:12:04 +0300

    Update inheritance

---
+++ Week2/Homeworks/Homework2/inheritance/src/inheritance/EmployeeManager.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 36
+
+public class EmployeeManager extends PersonManager {
+	
+	public void BestEmployee() {
+		System.out.println("Ayın elemanı getirildi.");
+	}
+	
+}
---
+++ Week2/Homeworks/Homework2/inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+// https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+// "JAVA Dersleri"
+// Lesson 36
+
+public class PersonManager {
+	
+	public void List() {
+		System.out.println("Listelendi.");
+	}
+	
+	public void Add() {
+		System.out.println("Eklendi.");
+	}
+	
+}
