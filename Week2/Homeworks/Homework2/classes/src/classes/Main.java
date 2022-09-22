8fc9e0773c3d14331466b36aa200d3b14495b0d3
emrecanstk <emrecanstk@gmail.com>
2022-09-22 19:28:26 +0300

    Update classes

--- Week2/Homeworks/Homework2/classes/src/classes/Main.java
+++ Week2/Homeworks/Homework2/classes/src/classes/Main.java
@@ -9,11 +9,22 @@ public class Main {
 		customerManager.Add();
 		customerManager.Remove();
 		customerManager.Update();
-		
-		
+
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi2 = sayi1;
+		sayi1 = 30;
+
+		System.out.println(sayi2);
+
+		int[] sayilar1 = new int[] { 1, 2, 3 };
+		int[] sayilar2 = new int[] { 4, 5, 6 };
+
+		sayilar2 = sayilar1;
+		sayilar1[0] = 10;
+
+		System.out.println(sayilar2[0]);
 
 	}
-	
-	
 
 }
