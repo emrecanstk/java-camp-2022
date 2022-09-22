6a4a8729cb14546bd4dc743294662d6dd03cf600
emrecanstk <emrecanstk@gmail.com>
2022-09-22 19:11:12 +0300

    Update methods2

--- Week2/Homeworks/Homework2/methods2/src/methods2/Main.java
+++ Week2/Homeworks/Homework2/methods2/src/methods2/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		String mesaj = "Bugün hava çok güzel";
-		
+
 		int sayi = topla(15, 4);
 		System.out.println(sayi);
 
+		int toplam = topla2(2, 3, 8, 7, 6, 4, 5);
+		System.out.println(toplam);
 	}
 
 	public static void ekle() {
@@ -20,6 +22,14 @@ public class Main {
 
 	public static void guncelle() {
 		System.out.println("Güncellendi");
+	}
+
+	public static int topla2(int... sayilar) {
+		int toplam = 0;
+		for (int sayi : sayilar) {
+			toplam += sayi;
+		}
+		return toplam;
 	}
 
 	public static int topla(int sayi1, int sayi2) {
