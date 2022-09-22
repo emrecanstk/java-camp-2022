0b34adce10a49320b60175a950d7465b9a2f9f77
emrecanstk <emrecanstk@gmail.com>
2022-09-22 19:19:35 +0300

    Add classes

---
+++ Week2/Homeworks/Homework2/classes/src/classes/CustomerManager.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class CustomerManager {
+	public void Add() {
+		System.out.println("Müşteri eklendi.");
+	}
+
+	public void Remove() {
+		System.out.println("Müşteri silindi.");
+	}
+	
+	public void Update() {
+		System.out.println("Müşteri güncellendi.");
+	}
+}
