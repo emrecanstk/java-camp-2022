dfb496dcd53f0580e706329bb34e9cab858967bb
emrecanstk <emrecanstk@gmail.com>
2022-10-05 22:43:19 +0300

    Add constructer and getters-setters

--- Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/entities/Product.java
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/entities/Product.java
@@ -5,4 +5,40 @@ public class Product {
 	private String name;
 	private double unitPrice;
 	
+	public Product() {
+		super();
+	}
+
+	public Product(int id, String name, double unitPrice) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	
 }
