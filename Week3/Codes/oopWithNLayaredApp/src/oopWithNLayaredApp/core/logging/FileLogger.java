0264d8d925858ce94f76e3352b4e1f5ae03930ee
emrecanstk <emrecanstk@gmail.com>
2022-10-05 23:20:42 +0300

    Add FileLogger

---
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/core/logging/FileLogger.java
@@ -0,0 +1,11 @@
+package oopWithNLayaredApp.core.logging;
+
+public class FileLogger implements Logger {
+
+	@Override
+	public void log(String data) {
+		System.out.println("Dosyaya loglandı: "+data);
+		
+	}
+
+}
