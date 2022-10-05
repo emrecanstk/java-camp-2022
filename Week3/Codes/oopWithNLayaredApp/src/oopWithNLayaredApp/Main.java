f8ba221dd43105b2dfa643cd3fa8612c9131fc05
emrecanstk <emrecanstk@gmail.com>
2022-10-05 22:42:00 +0300

    Add Main and Product classes

---
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/Main.java
@@ -0,0 +1,10 @@
+package oopWithNLayaredApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/entities/Product.java
@@ -0,0 +1,8 @@
+package oopWithNLayaredApp.entities;
+
+public class Product {
+	private int id;
+	private String name;
+	private double unitPrice;
+	
+}
