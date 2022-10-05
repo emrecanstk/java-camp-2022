2a5ace1bcc32d55b4683e0d1da80562eb7fb3379
emrecanstk <emrecanstk@gmail.com>
2022-10-05 23:02:11 +0300

    Add a test in Main

--- Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/Main.java
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/Main.java
@@ -1,9 +1,15 @@
 package oopWithNLayaredApp;
 
+import oopWithNLayaredApp.business.ProductManager;
+import oopWithNLayaredApp.entities.Product;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Product product1 = new Product(1,"Poco X3",10000);
 		
+		ProductManager productManager = new ProductManager();
+		productManager.add(product1);
 
 	}
 
