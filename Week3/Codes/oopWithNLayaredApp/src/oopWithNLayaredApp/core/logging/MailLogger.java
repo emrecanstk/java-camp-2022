7c9b4fbf31392d0384721dfd3ea564b039ce45ac
emrecanstk <emrecanstk@gmail.com>
2022-10-05 23:22:42 +0300

    Add MailLogger

---
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/core/logging/MailLogger.java
@@ -0,0 +1,11 @@
+package oopWithNLayaredApp.core.logging;
+
+public class MailLogger implements Logger {
+
+	@Override
+	public void log(String data) {
+		System.out.println("Mail gönderildi: "+data);
+		
+	}
+
+}
