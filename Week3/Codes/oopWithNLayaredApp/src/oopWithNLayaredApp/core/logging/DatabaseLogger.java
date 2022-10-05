fd52303f17b546ea045848d9c11f041b57d93dca
emrecanstk <emrecanstk@gmail.com>
2022-10-05 23:22:08 +0300

    Add DatabaseLogger

---
+++ Week3/Codes/oopWithNLayaredApp/src/oopWithNLayaredApp/core/logging/DatabaseLogger.java
@@ -0,0 +1,11 @@
+package oopWithNLayaredApp.core.logging;
+
+public class DatabaseLogger implements Logger {
+
+	@Override
+	public void log(String data) {
+		System.out.println("Veritabanına loglandı: "+data);
+		
+	}
+
+}
