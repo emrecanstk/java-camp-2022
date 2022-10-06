451cd1bad9ba1afe99f8365cda3d1ea69cf30454
emrecanstk <emrecanstk@gmail.com>
2022-10-06 14:54:27 +0300

    Add example for referances in list

--- Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Main.java
+++ Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Main.java
@@ -3,13 +3,18 @@ package youtubeEgitim;
 public class Main {
 
 	public static void main(String[] args) {
-		int sayi1 = 10;
-		int sayi2 = 20;
-		sayi1 = sayi2;
-		sayi2 = 100;
-		System.out.println(sayi1);
+		
+		int[] sayilar1 = {1,2,3};
+		int[] sayilar2 = {10,20,30};
+		
+		sayilar1 = sayilar2;
+		
+		sayilar2[0] = 100;
+		
+		System.out.println(sayilar1[0]);
 		
 		
 	}
 
 }
+ 
