6dde5a02930609efec97cb8531abe85749c380db
emrecanstk <emrecanstk@gmail.com>
2022-10-06 15:47:38 +0300

    Add CreditManager

---
+++ Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/CreditManager.java
@@ -0,0 +1,11 @@
+package youtubeEgitim;
+
+public class CreditManager {
+	public void Calculate() {
+		System.out.println("Hesaplandı.");
+	}
+
+	public void Save() {
+		System.out.println("Kredi verildi.");
+	}
+}
--- Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Main.java
+++ Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Main.java
@@ -3,18 +3,10 @@ package youtubeEgitim;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		int[] sayilar1 = {1,2,3};
-		int[] sayilar2 = {10,20,30};
-		
-		sayilar1 = sayilar2;
-		
-		sayilar2[0] = 100;
-		
-		System.out.println(sayilar1[0]);
-		
-		
+		CreditManager creditManager = new CreditManager();
+		creditManager.Calculate();
+		creditManager.Calculate();
+		creditManager.Save();
 	}
 
 }
- 
