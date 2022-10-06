0c61b69ece53aa6d6868ad405c95ea1cf8546f0b
emrecanstk <emrecanstk@gmail.com>
2022-10-06 14:52:41 +0300

    Add an example fot referances

--- Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Main.java
+++ Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Main.java
@@ -3,8 +3,13 @@ package youtubeEgitim;
 public class Main {
 
 	public static void main(String[] args) {
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi1 = sayi2;
+		sayi2 = 100;
+		System.out.println(sayi1);
 		
-
+		
 	}
 
 }
