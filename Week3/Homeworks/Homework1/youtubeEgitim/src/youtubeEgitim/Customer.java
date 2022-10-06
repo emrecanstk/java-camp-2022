a315ccbcd5571e8b04455eef70a123f54fc1723f
emrecanstk <emrecanstk@gmail.com>
2022-10-06 15:56:18 +0300

    Add Customer

---
+++ Week3/Homeworks/Homework1/youtubeEgitim/src/youtubeEgitim/Customer.java
@@ -0,0 +1,28 @@
+package youtubeEgitim;
+
+public class Customer {
+	private int id;
+	private String lastName;
+	private String nationalIdentity;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNationalIdentity() {
+		return nationalIdentity;
+	}
+	public void setNationalIdentity(String nationalIdentity) {
+		this.nationalIdentity = nationalIdentity;
+	}
+	
+	
+}
