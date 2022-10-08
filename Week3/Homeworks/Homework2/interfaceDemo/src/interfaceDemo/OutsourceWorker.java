2b65cc2f7a21602a154af832bd5fa656265c767d
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:40:27 +0300

    Add alternative Worker classes

---
+++ Week3/Homeworks/Homework2/interfaceDemo/src/interfaceDemo/OutsourceWorker.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+
+public class OutsourceWorker implements IWorkable {
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Week3/Homeworks/Homework2/interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+
+public class Worker implements IWorkable {  // bizim çalışanımız
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}  
+
+}
