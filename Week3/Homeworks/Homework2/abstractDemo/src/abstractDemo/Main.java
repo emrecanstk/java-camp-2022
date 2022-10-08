240f402c70ac9836f63df01cfe9ddf3f0dd1b9f5
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:14:05 +0300

    Add Main

---
+++ Week3/Homeworks/Homework2/abstractDemo/src/abstractDemo/Main.java
@@ -0,0 +1,11 @@
+package abstractDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager(new MySqlDatabaseManager());
+		customerManager.getCustomers();
+
+	}
+
+}
