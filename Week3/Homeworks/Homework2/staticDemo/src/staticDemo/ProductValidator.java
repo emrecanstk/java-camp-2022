49859523177a4aec5bfbbff5b977e723e2cec001
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:47:25 +0300

    Add Validator

---
+++ Week3/Homeworks/Homework2/staticDemo/src/staticDemo/ProductValidator.java
@@ -0,0 +1,11 @@
+package staticDemo;
+
+public class ProductValidator { // ürün doğrulayıcı
+	public boolean isValid(Product product) {
+		if (product.price > 0 && product.name.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
