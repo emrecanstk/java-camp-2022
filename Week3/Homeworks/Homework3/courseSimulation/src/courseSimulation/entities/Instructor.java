89bc4b10dad0202bed1dccfefb4a0b7ccdce2ab0
emrecanstk <emrecanstk@gmail.com>
2022-10-08 22:22:48 +0300

    Update methods & Add delete method

--- Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/entities/Instructor.java
+++ Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/entities/Instructor.java
@@ -1,16 +1,28 @@
 package courseSimulation.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import courseSimulation.business.CourseManager;
 
 public class Instructor extends User implements ICanProduce {
 	private String website;
-	
+	private List<Course> haveCourses = new ArrayList<>();
+
 	@Override
 	public void createCourse(CourseManager courseManager, Course course) {
-		
-		System.out.print("Kurs oluşturma isteği --> "+this.getName()+ " | "+course.getName()+" --> ");
-		courseManager.save(course);
-		
+
+		System.out.print("Kurs oluşturma isteği --> " + this.getName() + " | " + course.getName() + " --> ");
+		courseManager.save(course, haveCourses);
+
+	}
+
+	@Override
+	public void deleteCourse(CourseManager courseManager, Course course) {
+
+		System.out.print("Kurs silme isteği --> " + this.getName() + " | " + course.getName() + " --> ");
+		courseManager.delete(course, haveCourses);
+
 	}
 
 	public String getWebsite() {
