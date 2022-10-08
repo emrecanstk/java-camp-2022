21b4035f24ea112e9b844d52e5a4e7f495378105
emrecanstk <emrecanstk@gmail.com>
2022-10-08 14:48:44 +0300

    Delete final keyword

--- Week3/Homeworks/Homework2/overriding/src/overriding/BaseKrediManager.java
+++ Week3/Homeworks/Homework2/overriding/src/overriding/BaseKrediManager.java
@@ -1,7 +1,7 @@
 package overriding;
 
 public class BaseKrediManager {
-	public final double hesapla(double tutar) {       // eğer hiçbir yerde override edilmesini istemiyorsak
+	public double hesapla(double tutar) {       // eğer hiçbir yerde override edilmesini istemiyorsak
 		return tutar * 1.18;                          // final anahtar kelimesini kullanabiliriz.
 	}
 }
