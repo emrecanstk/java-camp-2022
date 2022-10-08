ec7387941bf8aed09c6dcb5b8c560b7173a0abe2
emrecanstk <emrecanstk@gmail.com>
2022-10-08 17:53:58 +0300

    Add overriding

---
+++ Week3/Homeworks/Homework2/abstractClasses/src/abstractClasses/GameCalculator.java
@@ -0,0 +1,10 @@
+package abstractClasses;
+
+public class GameCalculator {
+	public void hesapla() {
+		System.out.println("Puanınız: 100");
+	}
+	public void gameOver() {
+		System.out.println("Oyun bitti.");
+	}
+}
---
+++ Week3/Homeworks/Homework2/abstractClasses/src/abstractClasses/KidGameCalculator.java
@@ -0,0 +1,5 @@
+package abstractClasses;
+
+public class KidGameCalculator extends GameCalculator{
+	
+}
