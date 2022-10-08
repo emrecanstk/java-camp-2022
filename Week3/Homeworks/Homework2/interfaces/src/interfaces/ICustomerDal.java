ebf83ad43729bee6a59e0c93f2528b50368e2cb4
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:23:42 +0300

    Add alternative databases

--- Week3/Homeworks/Homework2/interfaces/src/interfaces/ICustomerDal.java
+++ Week3/Homeworks/Homework2/interfaces/src/interfaces/ICustomerDal.java
@@ -3,3 +3,7 @@ package interfaces;
 public interface ICustomerDal {
 	void add();
 }
+
+
+// Dal: Data Access Layer
+// Dao: Data Access Object
---
+++ Week3/Homeworks/Homework2/interfaces/src/interfaces/MySqlCustomerDal.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class MySqlCustomerDal implements ICustomerDal {
+
+	@Override
+	public void add() {
+		System.out.println("MySql eklendi");
+		
+	}
+
+}
