ac52663ad20ed34583f20fbe3c5dc679f79de71b
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:19:33 +0300

    Add Main & Add information about interfaces

---
+++ Week3/Homeworks/Homework2/interfaces/src/interfaces/Main.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+// interface'lerin en öne çıkan yanı:
+// bir sınıfın birden fazla interface'i implemente edebilmesidir.
+
+// inheritasyon işlemlerinde (kalıtımda) böyle bir durum söz konusu değildir.
