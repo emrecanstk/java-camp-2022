17bb95ea69ee7de1424cb90556e19e3e574cccef
emrecanstk <emrecanstk@gmail.com>
2022-10-08 14:40:36 +0300

    Add Main class

---
+++ Week3/Homeworks/Homework2/overriding/src/overriding/Main.java
@@ -0,0 +1,12 @@
+package overriding;
+
+public class Main {
+	public static void main(String[] args) {
+		BaseKrediManager[] krediManagers = new BaseKrediManager[] { new OgretmenKrediManager(),new TarimKrediManager() };
+		
+		for (BaseKrediManager krediManager : krediManagers) {
+			System.out.println(krediManager.hesapla(1800));
+		}
+	}
+
+}
