2a3a9b8d21eab4e85b8f302f8c5c823988674f46
emrecanstk <emrecanstk@gmail.com>
2022-10-08 21:25:30 +0300

    Add save tests

--- Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/Main.java
+++ Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/Main.java
@@ -1,9 +1,30 @@
 package courseSimulation;
 
-public class Main {
+import courseSimulation.business.CourseManager;
+import courseSimulation.dataAccess.Hibernate;
+import courseSimulation.entities.Course;
 
+public class Main {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Course course1 = new Course();
+		course1.setName("Java 2022");
+		course1.setPrice(50);
+		
+		Course course2 = new Course();
+		course2.setName("Java 2022");    
+		course2.setPrice(50);
+		
+		Course course3 = new Course();
+		course3.setName("C 2008");
+		course3.setPrice(50);
+		
+		CourseManager courseManager = new CourseManager(new Hibernate());
+		courseManager.save(course1);
+		courseManager.save(course2);    // başka isimle çakıştığı için eklenemeyecek.
+		courseManager.save(course3);
+		
 
 	}
 
