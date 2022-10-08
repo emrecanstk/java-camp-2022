f532e5b82070cbc36fab5dc065ffab042e8b70fc
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:43:28 +0300

    Add multiple implement demo

--- Week3/Homeworks/Homework2/interfaceDemo/src/interfaceDemo/Worker.java
+++ Week3/Homeworks/Homework2/interfaceDemo/src/interfaceDemo/Worker.java
@@ -1,9 +1,21 @@
 package interfaceDemo;
 
-public class Worker implements IWorkable {  // bizim çalışanımız
+public class Worker implements IWorkable,IEatable,IPayable {  // bizim çalışanımız
 
 	@Override
 	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pay() {
 		// TODO Auto-generated method stub
 		
 	}  
