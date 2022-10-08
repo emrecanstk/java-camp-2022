94ade8418fd606447f8931f4a14f683391cc2a60
emrecanstk <emrecanstk@gmail.com>
2022-10-08 17:56:29 +0300

    Add abstract method & Add final keyword

--- Week3/Homeworks/Homework2/abstractClasses/src/abstractClasses/GameCalculator.java
+++ Week3/Homeworks/Homework2/abstractClasses/src/abstractClasses/GameCalculator.java
@@ -1,10 +1,13 @@
 package abstractClasses;
 
-public class GameCalculator {
-	public void hesapla() {
-		System.out.println("Puanınız: 100");
-	}
-	public void gameOver() {
+public abstract class GameCalculator {
+	public abstract void hesapla();                  // gövdesi nerede yazılırsa o şekilde çalışacak olan bir method.
+
+	public final void gameOver() {                   // herkes için ortak. zorunlu bir method.  - final ile override olanağı kaldırıldı
 		System.out.println("Oyun bitti.");
 	}
 }
+
+
+// dolayısıyla kim bu sınıfı miras alıyor ise hesapla'yı override etmek zorunda!
+// gameOver'ı ise aynen kullanmak zorunda.
--- Week3/Homeworks/Homework2/abstractClasses/src/abstractClasses/KidGameCalculator.java
+++ Week3/Homeworks/Homework2/abstractClasses/src/abstractClasses/KidGameCalculator.java
@@ -1,5 +1,11 @@
 package abstractClasses;
 
 public class KidGameCalculator extends GameCalculator{
+
+	@Override    // annotation
+	public void hesapla() {
+		System.out.println("Puanınız: 100");
+		
+	}
 	
 }
