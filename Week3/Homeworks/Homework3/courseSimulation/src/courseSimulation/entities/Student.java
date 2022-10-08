509b91bd5969d3e7890f2b23610fd1b2ce435881
emrecanstk <emrecanstk@gmail.com>
2022-10-08 22:08:55 +0300

    Add getters&setters for gitHubAdress

--- Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/entities/Student.java
+++ Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/entities/Student.java
@@ -1,12 +1,19 @@
 package courseSimulation.entities;
 
 public class Student extends User implements ICanConsume {
-	private int numberOfCoursesTaken;
 	private String gitHubAdress;
 
 	@Override
 	public void takeCourse(Course course) {
 		System.out.println(this.getName()+ " öğrencisi "+course.getName()+" kursunu aldı.");
 		
+	}
+
+	public String getGitHubAdress() {
+		return gitHubAdress;
+	}
+
+	public void setGitHubAdress(String gitHubAdress) {
+		this.gitHubAdress = gitHubAdress;
 	}
 }
