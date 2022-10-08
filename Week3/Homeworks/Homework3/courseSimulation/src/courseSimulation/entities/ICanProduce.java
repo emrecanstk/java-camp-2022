6be74f83da8c2f5ac0bddab55b85c5f92708d21b
emrecanstk <emrecanstk@gmail.com>
2022-10-08 22:22:31 +0300

    Update method

--- Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/entities/ICanProduce.java
+++ Week3/Homeworks/Homework3/courseSimulation/src/courseSimulation/entities/ICanProduce.java
@@ -4,4 +4,5 @@ import courseSimulation.business.CourseManager;
 
 public interface ICanProduce {
 	void createCourse(CourseManager courseManager, Course course);
+	void deleteCourse(CourseManager courseManager, Course course);
 }
