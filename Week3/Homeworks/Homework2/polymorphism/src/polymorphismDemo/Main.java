50eabbaa36dc2847005735b355b90c7284681d3e
emrecanstk <emrecanstk@gmail.com>
2022-10-08 14:27:24 +0300

    Add ConsoleLogger

--- Week3/Homeworks/Homework2/polymorphism/src/polymorphismDemo/Main.java
+++ Week3/Homeworks/Homework2/polymorphism/src/polymorphismDemo/Main.java
@@ -3,8 +3,10 @@ package polymorphismDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		BaseLogger[] loggers = {new FileLogger(),new EmailLogger(),new DatabaseLogger()};
-
+		BaseLogger[] loggers = { new FileLogger(), new EmailLogger(), new DatabaseLogger(),new ConsoleLogger() };
+		for (BaseLogger logger : loggers) {
+			logger.log("Log message");
+		}
 	}
 
 }
