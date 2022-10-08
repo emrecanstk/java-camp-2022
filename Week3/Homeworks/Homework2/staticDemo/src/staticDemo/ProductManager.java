b99b89913d52fbbaadcc83b5f73335e3d617866b
emrecanstk <emrecanstk@gmail.com>
2022-10-08 18:54:33 +0300

    Add static keyword on Valitador

--- Week3/Homeworks/Homework2/staticDemo/src/staticDemo/ProductManager.java
+++ Week3/Homeworks/Homework2/staticDemo/src/staticDemo/ProductManager.java
@@ -2,8 +2,7 @@ package staticDemo;
 
 public class ProductManager {
 	public void add(Product product) {
-		ProductValidator productValidator = new ProductValidator();
-		if (productValidator.isValid(product)) {
+		if (ProductValidator.isValid(product)) {
 			System.out.println("Eklendi");
 		} else {
 			System.out.println("Ürün bilgileri geçersiz");
--- Week3/Homeworks/Homework2/staticDemo/src/staticDemo/ProductValidator.java
+++ Week3/Homeworks/Homework2/staticDemo/src/staticDemo/ProductValidator.java
@@ -1,7 +1,7 @@
 package staticDemo;
 
 public class ProductValidator { // ürün doğrulayıcı
-	public boolean isValid(Product product) {
+	public static boolean isValid(Product product) {
 		if (product.price > 0 && !(product.name.isEmpty())) {
 			return true;
 		} else {
