0d9991e37e5bb50b92d8aed2f4761d11da1f2fa7
emrecanstk <emrecanstk@gmail.com>
2022-10-10 16:10:28 +0300

    Add having, inner join, on

--- Week3/Homeworks/Homework5/SQLQuery2.sql
+++ Week3/Homeworks/Homework5/SQLQuery2.sql
@@ -22,6 +22,14 @@ select CategoryId,count(*) Adet from products group by CategoryID
 -- CategoryId'leri kendi içlerinde id'lerine göre grupla (tekrarsız şekilde tümünü yazdırmak)
 -- Adet sütun ismiyle ürünleri categoryId'lerine göre sayarak grupla.
 
+-- Having
 select CategoryId,count(*) Adet from products where UnitPrice > 20 group by CategoryID having count(*) < 10
 -- birim fiyatı 20'den büyük olanlar arasından count(*) olan kimülatif veri için
 -- 10'dan küçük olma şartıyla gruplandırarak saydırma yaptık
+
+-- Inner join , on
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName 
+from products inner join Categories on Products.CategoryID = Categories.CategoryID
+-- Ürünlerin id, isim ve birim fiyatı içerenlerini ve katagorilerin isiim içeren sütunlarını seç.
+-- Bu seçime ürünlerdeki katagori numaraları ile
+-- Katagorilerin katagori numaraları eşleşen katagorileri alma şartıyla ekle.
