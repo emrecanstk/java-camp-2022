688c80fa97fa5079e6538a02602361167a753278
emrecanstk <emrecanstk@gmail.com>
2022-11-02 21:48:19 +0300

    Add Jpa Repository

--- Week4/campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ Week4/campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,5 +1,6 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -7,13 +8,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Table(name="brands")   // tabloda name'i brands'e denk gelen sütunla eşleştiriyoruz.
+@Table(name="brands")   // tabloda name'i brands'e denk getiriyoruz.
 @Data                   // Lombok sayesinde getters setters eklemeye gerek kalmıyor.
 // Yalnızca getter'lar olsun istiyosak buraya @Getter yazıp @Data'yı silebilirdik.
 @AllArgsConstructor     // tüm parametreler içeren yapıcı metot
 @NoArgsConstructor      // parametresiz yapıcı metot
 @Entity                 // Sınıfın bir varlık (entity) olduğu bilgisi (anotasyon)
 public class Brand {
+	
+	@Column(name="id")    // Sütunları eşleştirdik
 	private int id;
+	
+	@Column(name="name")  // Sütunları eşleştirdik
 	private String name;
 }
