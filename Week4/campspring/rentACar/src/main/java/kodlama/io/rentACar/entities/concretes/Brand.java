54ec0bb610da487bf68a6a07573673eb31274087
emrecanstk <emrecanstk@gmail.com>
2022-11-02 21:34:56 +0300

    Add lombok on project firt time

--- Week4/campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ Week4/campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,33 +1,19 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="brands")   // tabloda name'i brands'e denk gelen sütunla eşleştiriyoruz.
+@Data                   // Lombok sayesinde getters setters eklemeye gerek kalmıyor.
+// Yalnızca getter'lar olsun istiyosak buraya @Getter yazıp @Data'yı silebilirdik.
+@AllArgsConstructor     // tüm parametreler içeren yapıcı metot
+@NoArgsConstructor      // parametresiz yapıcı metot
+@Entity                 // Sınıfın bir varlık (entity) olduğu bilgisi (anotasyon)
 public class Brand {
 	private int id;
 	private String name;
-	
-	public Brand() {
-		
-	}
-
-	public Brand(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
 }
