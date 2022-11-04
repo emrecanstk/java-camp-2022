62da13e25b2c47d6b99c919e09148c4eefdea684
emrecanstk <emrecanstk@gmail.com>
2022-11-04 15:08:03 +0300

    fix some problems about annotations

--- Week4/campspring/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ Week4/campspring/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
-import kodlama.io.rentACar.entities.concretes.Brand;
 
 @RestController
 @RequestMapping("/api/brands")
@@ -24,5 +25,10 @@ public class BrandsController {
 	@GetMapping("/getall")
 	public List<GetAllBrandsResponse> getAll() {
 		return brandService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public void add(CreateBrandRequest createBrandRequest) {
+		this.brandService.add(createBrandRequest);
 	}
 }
