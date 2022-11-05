ba4052647577ccd766ef3ee85cf04d4019f2da99
emrecanstk <emrecanstk@gmail.com>
2022-11-06 00:52:44 +0300

    Add lombok and column set

--- Devs/src/main/java/Kodlama/io/Devs/entities/concretes/PLanguage.java
+++ Devs/src/main/java/Kodlama/io/Devs/entities/concretes/PLanguage.java
@@ -1,29 +1,28 @@
 package Kodlama.io.Devs.entities.concretes;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="pLanguages")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class PLanguage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="name")
 	private String name;
-	
-	public PLanguage(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
 }
