2febac53b29220ebf3f78179e093cfec0f2e2413
emrecanstk <emrecanstk@gmail.com>
2022-11-06 00:53:03 +0300

    Add GetAll and Create

---
+++ Devs/src/main/java/Kodlama/io/Devs/business/requests/CreatePLanguageRequest.java
@@ -0,0 +1,12 @@
+package Kodlama.io.Devs.business.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreatePLanguageRequest {
+	private String name;
+}
